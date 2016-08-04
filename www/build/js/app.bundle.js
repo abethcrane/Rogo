7281baac2334893fa82b5949cd4dd4c7c0cb01ab
Charlton <charltonsmith@hotmail.com>
2016-08-03 18:47:16 -0700

    rebuilding the site to upload new items

--- www/build/js/app.bundle.js
+++ www/build/js/app.bundle.js
@@ -1166,7 +1166,7 @@ exports.CATEGORIES = [
         id: 1,
         name: "Mamma's Hands",
         thumb: "img/categories/accessories.png",
-        item_count: 2
+        item_count: 11
     }
 ];
 },{}],24:[function(require,module,exports){
@@ -1176,14 +1176,122 @@ exports.ITEMS = [
         id: 1,
         category_id: 1,
         organization: "Mamma's Hands",
-        name: "$1700 for Apartment Deposit",
+        price: 25,
+        name: "Gas Card",
         images: [
-            "img/items/detail/d_1.jpg",
-            "img/items/detail/d_2.jpg",
-            "img/items/detail/d_3.jpg",
-            "img/items/detail/d_4.jpg",
+            "img/items/detail/d_1.jpg"
         ],
-        short_description: "With this money we can pay the first month's rent to get somebody set up in an apartment",
+        short_description: "provide gas for a family for up to 1 week"
+    },
+    {
+        id: 2,
+        category_id: 1,
+        organization: "Mamma's Hands",
+        price: 25,
+        name: "Cell Service",
+        images: [
+            "img/items/detail/d_1.jpg"
+        ],
+        short_description: "Cell Phone Service for 1 month for 1 woman"
+    },
+    {
+        id: 3,
+        category_id: 1,
+        organization: "Mamma's Hands",
+        price: 250,
+        name: "Cleaning supplies",
+        images: [
+            "img/items/detail/d_1.jpg"
+        ],
+        short_description: "Cleaning Supplies for all 3 shelters for 1 month"
+    },
+    {
+        id: 4,
+        category_id: 1,
+        organization: "Mamma's Hands",
+        price: 35,
+        name: "Bed In Bag",
+        images: [
+            "img/items/detail/d_1.jpg"
+        ],
+        short_description: "Complete bedding for 1"
+    },
+    {
+        id: 5,
+        category_id: 1,
+        organization: "Mamma's Hands",
+        price: 50,
+        name: "Food for a family",
+        images: [
+            "img/items/detail/d_1.jpg"
+        ],
+        short_description: "Safeway or QFC Gift Card - 1 week of food for a family of 2"
+    },
+    {
+        id: 6,
+        category_id: 1,
+        organization: "Mamma's Hands",
+        price: 50,
+        name: "Phone + service",
+        images: [
+            "img/items/detail/d_1.jpg"
+        ],
+        short_description: "1 Go phone plus a month of service"
+    },
+    {
+        id: 7,
+        category_id: 1,
+        organization: "Mamma's Hands",
+        price: 100,
+        name: "Self Defense",
+        images: [
+            "img/items/detail/d_1.jpg"
+        ],
+        short_description: "Self Defense Class for 4 women"
+    },
+    {
+        id: 8,
+        category_id: 1,
+        organization: "Mamma's Hands",
+        price: 150,
+        name: "Storage for Personal Items",
+        images: [
+            "img/items/detail/d_1.jpg"
+        ],
+        short_description: "1 Hollon ES- 2046 Hotel Safe to be place in our resident rooms so families can have a place to protect their belongings while in the shelter"
+    },
+    {
+        id: 9,
+        category_id: 1,
+        organization: "Mamma's Hands",
+        price: 150,
+        name: "General purpose",
+        images: [
+            "img/items/detail/d_1.jpg"
+        ],
+        short_description: "Monetary Donation for miscellaneous items at our Non Profit"
+    },
+    {
+        id: 10,
+        category_id: 1,
+        organization: "Mamma's Hands",
+        price: 250,
+        name: "Shelter Printer",
+        images: [
+            "img/items/detail/d_1.jpg"
+        ],
+        short_description: "New Laser Jet Printer with WiFi for the Shelter"
+    },
+    {
+        id: 11,
+        category_id: 1,
+        organization: "Mamma's Hands",
+        price: 150,
+        name: "Storage for Personal Items",
+        images: [
+            "img/items/detail/d_1.jpg"
+        ],
+        short_description: "1 Hollon ES- 2046 Hotel Safe to be place in our resident rooms so families can have a place to protect their belongings while in the shelter"
     }
 ];
 },{}],25:[function(require,module,exports){
