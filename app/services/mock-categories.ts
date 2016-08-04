7a102a8376f984e824c221ec262ec7ff4613a68d
beth <abethcrane@gmail.com>
2016-08-03 17:04:55 -0700

    Initial useful mock data

--- app/services/mock-categories.ts
+++ app/services/mock-categories.ts
@@ -1,56 +1,8 @@
 export let CATEGORIES = [
   {
     id: 1,
-    name: "Apparel & Accessories",
+    name: "Mamma's Hands",
     thumb: "img/categories/accessories.png",
-    item_count: 923
-  },
-  {
-    id: 2,
-    name: "Electronics",
-    thumb: "img/categories/electronics.png",
-    item_count: 810
-  },
-  {
-    id: 3,
-    name: "Jewelry",
-    thumb: "img/categories/jewelry.png",
-    item_count: 102
-  },
-  {
-    id: 4,
-    name: "Automobiles & Motorcycles",
-    thumb: "",
-    item_count: 780
-  },
-  {
-    id: 5,
-    name: "Watches",
-    thumb: "",
-    item_count: 567
-  },
-  {
-    id: 6,
-    name: "Shoes",
-    thumb: "",
-    item_count: 888
-  },
-  {
-    id: 7,
-    name: "Luggage & Bags",
-    thumb: "",
-    item_count: 937
-  },
-  {
-    id: 8,
-    name: "Sports & Entertainment",
-    thumb: "",
-    item_count: 675
-  },
-  {
-    id: 9,
-    name: "Home & Garden",
-    thumb: "",
-    item_count: 970
+    item_count: 2
   }
 ]
